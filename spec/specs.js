c4fca6077bb6868ce5880ffbd81c8fd5157d4de6
Don Schemmel <dons500@gmail.com>
2015-09-28 10:30:20 -0700

    Add test for a medium pizza

--- spec/specs.js
+++ spec/specs.js
@@ -5,11 +5,18 @@ describe('Order', function() {
     expect(testOrder.pizzaSize).to.equal("small");
     expect(testOrder.topping).to.eql(["pepperoni"]);
   });
+
 });
 
 describe('Order.cost', function() {
-  it("charges $6 for a basic one topping pizza", function() {
+  it("charges $6 for a basic small one topping pizza", function() {
     var testOrder = new Order(1,"small",["pepperoni"]);
     expect(testOrder.cost()).to.equal(6);
   });
+
+  it("charges $8 for a basic medium one topping pizza", function() {
+    var testOrder = new Order(1,"medium",["pepperoni"]);
+    expect(testOrder.cost()).to.equal(8);
+  });
+
 });
